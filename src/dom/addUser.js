2ee8c81a42b97606cff69ff045a64dc56aa4803a
Dmitry <jathpr@gmail.com>
2021-07-08 18:58:54 +0300

    less simple

--- src/dom/addUser.js
+++ src/dom/addUser.js
@@ -1,6 +1,7 @@
 import { httpRequests } from "../requests/http.js";
 import $ from "jQuery";
-import { users, usersContainer } from "./showUsers.js";
+import { getUsers, users, usersContainer } from "./showUsers.js";
+import "./addUser.css";
 
 const http = httpRequests("http://localhost:3033/users");
 
@@ -8,8 +9,9 @@ const addInput = $("<input/>");
 
 const addButton = $("<button>Add</button>").on("click", () => {
   const newUserName = addInput.val();
-  users.push(newUserName);
-  usersContainer.trigger("create");
+  // getUsers();
+  // users.push(newUserName);
+  // usersContainer.trigger("create");
   http.post(newUserName);
 });
 
--- src/dom/showUsers.js
+++ src/dom/showUsers.js
@@ -1,7 +1,15 @@
 import $ from "jQuery";
+import { httpRequests } from "../requests/http.js";
 
 export const users = ["one", "two", "five"];
 
+const http = httpRequests("http://localhost:3033/users");
+
+export const getUsers = async () => {
+  const users = await http.get();
+  console.log("🚀 ~ file: showUsers.js ~ line 13 ~ getUsers ~ users", users);
+};
+getUsers();
 export const usersContainer = $("<ul></ul>")
   .append(users.map((user) => $(`<li>${user}</li>`)))
   .addClass("users_wrapper");
